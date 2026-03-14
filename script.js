d8044649bd1cc0cf58543f3f4621a6592c263155
0xWalmark <0xwalmark@gmail.com>
2026-03-14 12:21:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,107 +4,81 @@ const ctx = canvas.getContext("2d");
 let greenSlots = [];
 
 function loadBanner(){
+  let id = document.getElementById("tokenId").value;
 
-let id = document.getElementById("tokenId").value;
+  if(id < 1 || id > 2400){
+    alert("Token ID must be between 1 and 2400");
+    return;
+  }
 
-if(id < 1 || id > 2400){
-alert("Token ID must be between 1 and 2400");
-return;
-}
+  let img = new Image();
 
-let img = new Image();
+  img.onload = function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(img,0,0);
+    detectGreenSquares();
+  };
 
-img.onload = function(){
-
-ctx.clearRect(0,0,canvas.width,canvas.height);
-ctx.drawImage(img,0,0);
-
-detectGreenSquares();
-
-};
-
-img.src = "images/"+id+".png";
-
+  img.src = "images/"+id+".png"; // cartella corretta
 }
 
 function detectGreenSquares(){
+  greenSlots = [];
 
-greenSlots = [];
+  let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+  let data = imageData.data;
 
-let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-let data = imageData.data;
+  for(let y=0;y<canvas.height;y+=96){
+    for(let x=0;x<canvas.width;x+=96){
+      let index = (y*canvas.width + x) * 4;
+      let r = data[index];
+      let g = data[index+1];
+      let b = data[index+2];
 
-for(let y=0;y<canvas.height;y+=96){
-for(let x=0;x<canvas.width;x+=96){
+      if(r===0 && g===255 && b===30){
+        greenSlots.push({x,y});
+      }
+    }
+  }
 
-let index = (y*canvas.width + x) * 4;
-
-let r = data[index];
-let g = data[index+1];
-let b = data[index+2];
-
-if(r===0 && g===255 && b===30){
-
-greenSlots.push({x,y});
-
+  createUploadInputs();
 }
 
-}
-}
+function createUploadInputs() {
+  let area = document.getElementById("uploadArea");
+  area.innerHTML = "";
 
-createUploadInputs();
+  greenSlots.forEach((slot, i) => {
+    let container = document.createElement("div"); // div per griglia
 
-}
+    let label = document.createElement("p");
+    label.innerText = "Slot " + (i+1);
 
-function createUploadInputs(){
+    let input = document.createElement("input");
+    input.type = "file";
 
-let area = document.getElementById("uploadArea");
-area.innerHTML="";
+    input.onchange = (e) => {
+      let file = e.target.files[0];
+      let reader = new FileReader();
+      reader.onload = function(ev) {
+        let img = new Image();
+        img.onload = function() {
+          ctx.drawImage(img, slot.x, slot.y, 96, 96);
+        };
+        img.src = ev.target.result;
+      };
+      reader.readAsDataURL(file);
+    };
 
-greenSlots.forEach((slot,i)=>{
-
-let label=document.createElement("p");
-label.innerText="Upload image for slot "+(i+1);
-
-let input=document.createElement("input");
-input.type="file";
-
-input.onchange=(e)=>{
-
-let file=e.target.files[0];
-
-let reader=new FileReader();
-
-reader.onload=function(ev){
-
-let img=new Image();
-
-img.onload=function(){
-
-ctx.drawImage(img,slot.x,slot.y,96,96);
-
-};
-
-img.src=ev.target.result;
-
-};
-
-reader.readAsDataURL(file);
-
-};
-
-area.appendChild(label);
-area.appendChild(input);
-
-});
-
+    container.appendChild(label);
+    container.appendChild(input);
+    area.appendChild(container);
+  });
 }
 
 function downloadBanner(){
-
-let link=document.createElement("a");
-link.download="worldwebwindows.png";
-link.href=canvas.toDataURL();
-link.click();
-
+  let link = document.createElement("a");
+  link.download = "worldwebwindows.png";
+  link.href = canvas.toDataURL();
+  link.click();
 }
