46b6001767a83672beca6f258da99f660eb5434e
0xWalmark <0xwalmark@gmail.com>
2026-03-14 12:32:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const canvasDownload = document.getElementById("canvasDownload");
+const ctxDownload = canvasDownload.getContext("2d");
+
 let greenSlots = [];
 let bannerImage = new Image();
 let currentToken = 0;
 
-// scala tra canvas visualizzato e reale
-function getCanvasScale() {
-    return canvas.clientWidth / canvas.width;
-}
-
+// carica banner selezionato
 function loadBanner(){
   let id = parseInt(document.getElementById("tokenId").value);
   if(isNaN(id) || id < 1 || id > 2400){
@@ -20,16 +19,23 @@ function loadBanner(){
 
   bannerImage = new Image();
   bannerImage.onload = function(){
+    // canvas visualizzato
     ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.drawImage(bannerImage,0,0);
+
+    // canvas download (reale)
+    ctxDownload.clearRect(0,0,canvasDownload.width,canvasDownload.height);
+    ctxDownload.drawImage(bannerImage,0,0);
+
     detectGreenSquares();
   };
   bannerImage.src = "images/"+id+".png";
 }
 
+// trova quadrati verdi 96x96
 function detectGreenSquares(){
   greenSlots = [];
-  let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+  let imageData = ctxDownload.getImageData(0,0,canvas.width,canvas.height);
   let data = imageData.data;
 
   for(let y=0;y<canvas.height;y+=96){
@@ -47,6 +53,7 @@ function detectGreenSquares(){
   createUploadInputs();
 }
 
+// crea pulsanti upload in griglia
 function createUploadInputs() {
   let area = document.getElementById("uploadArea");
   area.innerHTML = "";
@@ -66,10 +73,10 @@ function createUploadInputs() {
       reader.onload=function(ev){
         let img = new Image();
         img.onload = function(){
-          let scale = getCanvasScale();
-          // disegna sempre sulle coordinate originali, proporzionate alla scala del canvas visualizzato
-          ctx.clearRect(slot.x, slot.y, 96, 96); // pulisce eventuali precedenti
+          // disegna sia su canvas visualizzato che download
+          ctx.clearRect(slot.x, slot.y, 96, 96);
           ctx.drawImage(img, slot.x, slot.y, 96, 96);
+          ctxDownload.drawImage(img, slot.x, slot.y, 96, 96);
         };
         img.src = ev.target.result;
       };
@@ -82,90 +89,10 @@ function createUploadInputs() {
   });
 }
 
-function downloadBanner(){
-  // ricrea un canvas temporaneo reale per il download
-  let tmpCanvas = document.createElement("canvas");
-  tmpCanvas.width = canvas.width;
-  tmpCanvas.height = canvas.height;
-  let tmpCtx = tmpCanvas.getContext("2d");
-
-  // disegna il banner originale
-  tmpCtx.drawImage(bannerImage,0,0);
-
-  // ridisegna tutte le immagini caricate sui riquadri verdi
-  let inputs = document.querySelectorAll("#uploadArea input");
-  greenSlots.forEach((slot,i)=>{
-    if(inputs[i] && inputs[i].files[0]){
-      let file = inputs[i].files[0];
-      let reader = new FileReader();
-      reader.onload = function(ev){
-        let img = new Image();
-        img.onload = function(){
-          tmpCtx.drawImage(img, slot.x, slot.y, 96, 96);
-          // quando ultima immagine caricata, scarica
-          if(i===greenSlots.length-1){
-            let link = document.createElement("a");
-            link.download = "worldwebwindows.png";
-            link.href = tmpCanvas.toDataURL();
-            link.click();
-          }
-        };
-        img.src = ev.target.result;
-      };
-      reader.readAsDataURL(file);
-    } else if(i===greenSlots.length-1){
-      // se ultimi slot vuoti, scarica comunque
-      let link = document.createElement("a");
-      link.download = "worldwebwindows.png";
-      link.href = tmpCanvas.toDataURL();
-      link.click();
-    }
-  });
-}
-      if(r===0 && g===255 && b===30){
-        greenSlots.push({x,y});
-      }
-    }
-  }
-
-  createUploadInputs();
-}
-
-function createUploadInputs() {
-  let area = document.getElementById("uploadArea");
-  area.innerHTML = "";
-
-  greenSlots.forEach((slot, i) => {
-    let container = document.createElement("div"); // div per griglia
-
-    let label = document.createElement("p");
-    label.innerText = "Slot " + (i+1);
-
-    let input = document.createElement("input");
-    input.type = "file";
-
-    input.onchange = (e) => {
-      let file = e.target.files[0];
-      let reader = new FileReader();
-      reader.onload = function(ev) {
-        let img = new Image();
-        img.onload = function() {
-          ctx.drawImage(img, slot.x, slot.y, 96, 96);
-        };
-        img.src = ev.target.result;
-      };
-      reader.readAsDataURL(file);
-    };
-
-    container.appendChild(label);
-    container.appendChild(input);
-    area.appendChild(container);
-  });
-}
-
+// scarica banner finale
 function downloadBanner(){
   let link = document.createElement("a");
   link.download = "worldwebwindows.png";
-  link.href = canvas.toDataURL();
+  link.href = canvasDownload.toDataURL();
   link.click();
 }
